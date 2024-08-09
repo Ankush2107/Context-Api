7544e1907c5468fee959d93ec7c6a270bc8fe7f4
Ankush2107 <ankush.kumar210701@gmail.com>
2024-08-09 12:55:55 +0530

    understanding context api

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import { counterContext } from "../context/counter"
+
+function Button() {
+    let {count, setCount} = useContext(counterContext)
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Click me {count}</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, {  useContext } from 'react'
+import { counterContext } from '../context/counter'
+
+function Home() {
+    let { count, setCount } = useContext(counterContext);
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+export default Home
